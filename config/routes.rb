5028bb3f3f9a95ed916ccb34faedf668d4b2b932
artrsyf <rom.fadeev2017@yandex.ru>
2022-12-02 00:15:19 +0300

    styles for basic devise views, added overriden registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'comments/create'
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/registrations'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :posts do
     resources :comments
