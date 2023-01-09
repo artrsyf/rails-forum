d939a36d6b8a27941fea0866184c03384fc612e1
artrsyf <rom.fadeev2017@yandex.ru>
2023-01-09 20:18:04 +0300

    added ability to change localisation, but need some styles and new vocabulary

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,26 @@
 Rails.application.routes.draw do
-  get 'comments/create'
-  resources :profiles
-  resources :rooms do
-    resources :messages
-  end
-  post 'rooms/create'
-  post 'reposts/create'
-  post 'posts_controller/index/:post_index' => 'posts_controller#index'
-  devise_for :users, controllers: { registrations: 'registrations'}
-  get '/user' => "profiles#create", :as => :user_root
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :posts do
-    resources :comments
-    member do
-      post 'upvote', to: 'posts#upvote'
+  scope '(:locale)', locale: /#{I18n.available_locales.join("|")}/ do
+    get 'comments/create'
+    resources :profiles
+    resources :rooms do
+      resources :messages
     end
+    post 'rooms/create'
+    post 'reposts/create'
+    post 'posts_controller/index/:post_index' => 'posts_controller#index'
+    devise_for :users, controllers: { registrations: 'registrations'}
+    get '/user' => "profiles#create", :as => :user_root
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+    resources :posts do
+      resources :comments
+      member do
+        post 'upvote', to: 'posts#upvote'
+      end
+    end
+    post 'posts/repost'
+    root 'posts#index'
+    post 'messages/create_repost_message'
+    # Defines the root path route ("/")
+    # root "articles#index"
   end
-  post 'posts/repost'
-  root 'posts#index'
-  post 'messages/create_repost_message'
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
